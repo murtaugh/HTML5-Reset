54c88777751ff687b135403edf25ad5def0c46fc
Tim Murtaugh <tim@monkeydo.biz>
2010-10-05 16:43:07 +0000

    Uploaded individual packages

---
+++ html5reset-kitchensink/_/js/functions.js
@@ -0,0 +1,22 @@
+/* Declare a namespace for the site */
+var Site = window.Site || {};
+
+/* Create a closure to maintain scope of the '$'
+   and remain compatible with other frameworks.  */
+(function($) {
+	
+	//same as $(document).ready();
+	$(function() {
+		
+		
+
+	});
+
+
+	$(window).bind("load", function() {
+		
+		
+	
+	});
+	
+})(jQuery);
