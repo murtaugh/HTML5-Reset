0382e7f5a6f0ce9dd6c91062fc2672b6a88520e7
Tim Murtaugh <tim@monkeydo.biz>
2011-06-20 10:20:42 -0400

    Updated with better structure and comments. Also added optional load() and resize() blocks.

--- _/js/functions.js
+++ _/js/functions.js
@@ -1,14 +1,23 @@
 // remap jQuery to $
-(function($){
-
- 
+(function($){})(window.jQuery);
 
 
+/* trigger when page has loaded */
+$(document).ready(function (){
+
+	// your functions go here
+
+});
 
 
+/* optional triggers
 
- 
+$(window).load(function() {
+	
+});
 
+$(window).resize(function() {
+	
+});
 
-
-})(window.jQuery);
+*/
