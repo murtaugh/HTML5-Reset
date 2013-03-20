ad2d7041d93ccc5f33d4ec13901d488bd32cde83
Tim Murtaugh <tim@monkeydo.biz>
2013-03-20 16:03:58 -0400

    added Rogie's browser detection script (commented out)

--- _/js/functions.js
+++ _/js/functions.js
@@ -1,3 +1,13 @@
+// Browser detection for when you get desparate.
+// http://rog.ie/post/9089341529/html5boilerplatejs
+
+// var b = document.documentElement;
+// b.setAttribute('data-useragent',  navigator.userAgent);
+// b.setAttribute('data-platform', navigator.platform);
+
+// sample CSS: html[data-useragent*='Chrome/13.0'] { ... }
+
+
 // remap jQuery to $
 (function($){
 
