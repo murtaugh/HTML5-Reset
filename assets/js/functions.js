abbe64c47015dbb5690f0b3178fe706e8963ffbd
Tim Murtaugh <tim@o2b.net>
2014-02-28 11:47:14 -0500

    relocate to new assets folder

---
+++ assets/js/functions.js
@@ -0,0 +1,34 @@
+// Browser detection for when you get desparate. A measure of last resort.
+// http://rog.ie/post/9089341529/html5boilerplatejs
+
+// var b = document.documentElement;
+// b.setAttribute('data-useragent',  navigator.userAgent);
+// b.setAttribute('data-platform', navigator.platform);
+
+// sample CSS: html[data-useragent*='Chrome/13.0'] { ... }
+
+
+// remap jQuery to $
+(function($){
+
+	/* trigger when page is ready */
+	$(document).ready(function (){
+	
+		// your functions go here
+	
+	});
+	
+	
+	/* optional triggers
+	
+	$(window).load(function() {
+		
+	});
+	
+	$(window).resize(function() {
+		
+	});
+	
+	*/
+
+})(window.jQuery);
