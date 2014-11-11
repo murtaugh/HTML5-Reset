2bed05c53cfe6f957df6c575abbbb002e3443a6a
Tim Murtaugh <tim@o2b.net>
2014-11-11 13:16:21 -0500

    more sensible formatting for the browser detection snippet

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,11 +1,10 @@
 // Browser detection for when you get desparate. A measure of last resort.
 // http://rog.ie/post/9089341529/html5boilerplatejs
-
+// sample CSS: html[data-useragent*='Chrome/13.0'] { ... }
+//
 // var b = document.documentElement;
 // b.setAttribute('data-useragent',  navigator.userAgent);
 // b.setAttribute('data-platform', navigator.platform);
-
-// sample CSS: html[data-useragent*='Chrome/13.0'] { ... }
 
 
 // remap jQuery to $
