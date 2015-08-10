224aa0bd9763c815421fca4a05e2339ccaecb874
Tim Murtaugh <tim@o2b.net>
2015-08-10 14:52:32 -0400

    quick instruction for rog.ie's browser detection

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,9 @@
 // Browser detection for when you get desparate. A measure of last resort.
+
 // http://rog.ie/post/9089341529/html5boilerplatejs
 // sample CSS: html[data-useragent*='Chrome/13.0'] { ... }
-//
+
+// Uncomment the below to use:
 // var b = document.documentElement;
 // b.setAttribute('data-useragent',  navigator.userAgent);
 // b.setAttribute('data-platform', navigator.platform);
