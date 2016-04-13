9b8f54133ec71bc2aa23dfebb17d502c737185f7
Adrian Sieber <mail@adriansieber.com>
2016-04-13 19:52:57 +0000

    Fix typos

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,4 @@
-// Browser detection for when you get desparate. A measure of last resort.
+// Browser detection for when you get desperate. A measure of last resort.
 
 // http://rog.ie/post/9089341529/html5boilerplatejs
 // sample CSS: html[data-useragent*='Chrome/13.0'] { ... }
